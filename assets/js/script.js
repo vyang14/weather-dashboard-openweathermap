4af8279dfa5e3f35ae651ef23ea712f571df732d
vyang14 <vyang1014888@gmail.com>
2022-08-02 00:25:36 -0400

    fetch call working properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var timeEl = document.querySelector('#time');
 var searchBtn = document.querySelector('#searchBtn');
 var searchHistory = document.querySelector('#searchHistory');
+var bigCity = document.querySelector('#bigCity');
+var cityTemp = document.querySelector('#temperature');
+var cityWind = document.querySelector('#wind');
+var cityHumid = document.querySelector('#humidity');
+var uvIndex = document.querySelector('#uvIndex');
 
 function theTimeIsNow() { // displays the time in the search results box
     var currentTime = moment().format('ddd, MMM Do YYYY, hh:mm:ss a');
@@ -9,10 +14,28 @@ function theTimeIsNow() { // displays the time in the search results box
 
 setInterval(theTimeIsNow, 1000);
 
-function citySearch (city) {
-    
+function citySearch (event) {
+    event.preventDefault();
+    var cityInput = this.previousElementSibling.value;
 
-}
+    if (cityInput === "") {
+        window.alert("Invalid entry. Please press OK and try again.");
+        return;
+    } else {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=629118eb1a8773a241db2bc4f0a52be4"), {  headers: {
+            Accept: "application/json",
+    }
+    .then(function(response){
+        if(!response.ok){
+            newButton.textContent = `No results for ${cityInput}.`;
+            throw response.json();
+        }
+        return response.json();
+        .then(function(weatherData){
+            bigCity = weatherData.;
+        })
+    })
+}}}
 
 searchBtn.addEventListener("click", function(){
     var cityInput = this.previousElementSibling.value;
