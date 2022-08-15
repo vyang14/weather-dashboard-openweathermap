c9922f375d226458e61a1980a0a389d9c43b28cc
vyang14 <vyang1014888@gmail.com>
2022-08-15 01:37:59 -0400

    pulling from local storage and creating new buttons when user initiates search

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,39 +5,38 @@ var bigCity = document.querySelector('#bigCity');
 var cityTemp = document.querySelector('#temperature');
 var cityWind = document.querySelector('#wind');
 var cityHumid = document.querySelector('#humidity');
+var cityIcon = document.querySelector('#icon');
 var searchBar = document.querySelector('customSearch');
-var cityCoord = {
-    lat: '',
-    long: ''
-};
 var aKey = "&appid=629118eb1a8773a241db2bc4f0a52be4";
-// var fromStorage = JSON.parse(localStorage.getItem("weatherDashboard")) || [];
 
-function theTimeIsNow() { // displays the time in the search results box
+function theTimeIsNow() { //displays the time in the search results box
     var currentTime = moment().format('ddd, MMM Do YYYY, hh:mm:ss a');
     timeEl.textContent = currentTime;
 };
 
-setInterval(theTimeIsNow, 1000); //updates date and time every second
-
-function localStore (cityInput){
+function recentButton (cityInput){ //creates a new button containing the text from the search bar
     var newButton = document.createElement('button');
     newButton.classList.add('btn-dark','customBtn');
     newButton.setAttribute('id', cityInput);
     newButton.textContent = cityInput;
-    newButton.addEventListener("click", citySearch);
-    console.log(`newButton created.`);
+    newButton.addEventListener("click", function(){
+        var cityInput = this.textContent;
+        citySearch(cityInput);
+    });
     searchHistory.appendChild(newButton);
-    console.log(`newButton appended.`);
-
 }
 
-function citySearch (event) { //searches for city and fetches weather data using value in searchBar
-    var cityInput = event;
+function localStore (){ //pulls the values in local storage and calls recentButton for each value
+    var fromStorage = JSON.parse(localStorage.getItem("weatherDashboard")) || [];
+    for (var i = 0; i < fromStorage.length; i++){
+        recentButton(fromStorage[i]);
+    }
+}
+
+function citySearch (cityInput) { //searches for city and fetches weather data using value in searchBar
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial" + aKey)
     .then(function(response){
     if(response.ok){
-        console.log(`${cityInput} received. Returning response`);
         return response.json();
     } else {
         console.log(response);
@@ -47,11 +46,13 @@ function citySearch (event) { //searches for city and fetches weather data using
     })
     .then(function(weatherData){
         console.log(`returning weather data`);
+        var icon = weatherData.weather[0].icon;
         bigCity.textContent = `${weatherData.name}, ${weatherData.sys.country}`;
+        cityIcon.innerHTML = `<img src="https://openweathermap.org/img/w/${icon}.png">`
         cityTemp.textContent = `${weatherData.main.temp}°F`;
         cityWind.textContent = `${weatherData.wind.speed} mph`;
         cityHumid.textContent = `${weatherData.main.humidity}%`;
-        cityCoord = { lat: weatherData.coord.lat, long: weatherData.coord.lon };
+        var cityCoord = { lat: weatherData.coord.lat, long: weatherData.coord.lon };
         var fromStorage = JSON.parse(localStorage.getItem("weatherDashboard")) || [];
         fromStorage.push(cityInput);
         localStorage.setItem('weatherDashboard', JSON.stringify(fromStorage));
@@ -59,23 +60,20 @@ function citySearch (event) { //searches for city and fetches weather data using
     });
 }
 
-
 function fiveForecastUV (lat, long){ //grabs the UVindex and creates the cards for the 5-day forecast
-debugger
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial" + aKey)
     .then(function(response){
         return response.json();  
     })
     .then(function(weatherData){
+        var uvIndex = document.getElementById('uvIndex');
         let uvi = weatherData.current.uvi;
-        var uvIndex = document.querySelector('#uvIndex');
-        switch (uvi){
-            case uvi >= 0 && uvi <= 2:
-
-        }
-        
+        if (uvi >= 0 && uvi <= 2){ uvIndex.classList.add('uvGreen'); } 
+        else if (uvi >= 3 && uvi <= 5){ uvIndex.classList.add('uvYellow'); } 
+        else if (uvi >= 6 && uvi <= 7){ uvIndex.classList.add('uvOrange'); } 
+        else if (uvi >= 8 && uvi <= 10){ uvIndex.classList.add('uvRed'); } 
+        else { uvIndex.classList.add('uvPurple'); }
         uvIndex.textContent = uvi;
-        }
         for (i = 0; i < 5; i++){
             var newCard = document.getElementById('day' + (i + 1));
             var date = moment.unix(weatherData.daily[i].dt).format('MMM Do YYYY')
@@ -83,7 +81,7 @@ debugger
             var description = weatherData.daily[i].weather[0].main;
             var temp = weatherData.daily[i].temp.day;
             var humidity = weatherData.daily[i].humidity;
-            var dailyCard = `<h3>${date}</h3><br><img src="https://openweathermap.org/img/w/${icon}.png"><br><p>${description}</p><p>Temp: ${temp}°F</p><p>${humidity}%</p>`
+            var dailyCard = `<h3>${date}</h3><br><img src="https://openweathermap.org/img/w/${icon}.png"><br><p>${description}</p><p>Temp: ${temp}°F</p><p>Humidity: ${humidity}%</p>`
             newCard.innerHTML = dailyCard;
         }
     })
@@ -96,6 +94,10 @@ searchBtn.addEventListener("click", function(event){ //initiates app functionali
         window.alert("Invalid entry. Please press OK and try again.");
         return;
     } else {
+    recentButton(cityInput);
     citySearch(cityInput);
     }
 })
+
+setInterval(theTimeIsNow, 1000); //updates date and time every second
+localStore();
